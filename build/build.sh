d834ced3745c87a84d97b29a573d12f6f75e26b7
deseven <de7@deseven.info>
2016-07-18 18:30:08 +0200

    updated build script

--- build/build.sh
+++ build/build.sh
@@ -2,25 +2,43 @@
 
 pb="/Applications/PureBasic"
 name="iCanHazShortcut"
+shortName="ichs"
 ident="info.deseven.icanhazshortcut"
 loc="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+noColor='\033[0m'
+greenColor='\033[32m'
+redColor='\033[31m'
 
 cd "$loc"
 export PUREBASIC_HOME="$pb"
 rm -rf "$loc/$name.app"
+rm -rf "$loc/$shortName.zip"
+
+die() {
+	echo -e $redColor"\n$1"$noColor
+	exit 1
+}
 
 if [ -f "$pb/compilers/pbcompiler" ]; then
-	"$pb/compilers/pbcompiler" -u -e "$loc/$name.app" "$loc/../main.pb"
+	echo -ne $greenColor"compiling $shortName..."$noColor
+	"$pb/compilers/pbcompiler" -u -e "$loc/$name.app" "$loc/../main.pb" > /dev/null || die "failed to build $shortName"
 	if [ -d "$loc/$name.app" ]; then
+		echo -ne $greenColor"\ninjecting resources..."$noColor
 		cd ..
-		build/inject.sh "$loc/$name.app"
+		build/inject.sh "$loc/$name.app" || die "failed to inject $shortName"
+		echo -ne $greenColor"\nsigning bundle..."$noColor
 		if [ ! -z "$1" ]; then
 			# app signing
-			codesign -f -s $1 "$loc/$name.app" -r="host => anchor apple and identifier com.apple.translate designated => identifier $ident"
+			codesign -f -s $1 "$loc/$name.app" -r="host => anchor apple and identifier com.apple.translate designated => identifier $ident" > /dev/null || die "failed to sign $shortName"
 		fi
+		echo -ne $greenColor"\npacking distro..."$noColor
+		cd "$loc"
+		zip -r9 "$shortName.zip" "$name.app" > /dev/null || die "failed to pack $shortName"
+		echo
 	else
-		echo "build failed"
+		echo -e $redColor"bundle not found"$noColor
 	fi
 else
-	echo "can't find PB here: $pb"
+	echo -e $redColor"can't find PB here: $pb"$noColor
 fi
